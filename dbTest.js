b8c78d63f04ea8089569b6b61258ee4134ae59a2
connorGA <ronnoccap@gmail.com>
2022-09-14 22:44:16 -0700

    chore: setup dbtest.js and tested favorite model

--- dbTest.js
+++ dbTest.js
@@ -1,89 +1,131 @@
 const db = require('./models');
+const axios = require('axios');
+const { response } = require('express');
+
+// axios.get('https://genius.p.rapidapi.com/songs/')
+// .then(response => {
+//     console.log(response.data);
+// })
 
 // Implement CRUD for user model
 
-// CREATE
-async function createUser() {
-    try {
-        const newUser = await db.user.create({
-            name: "My Name",
-            email: "myemail@gmail.com"
-        });
-        console.log('my new user >>>', newUser);
-    } catch (error) {
-        console.log('new user was not created b/c of >>>', error);
-    }
+// // CREATE
+// async function createUser() {
+//     try {
+//         const newUser = await db.user.create({
+//             name: "My Name",
+//             email: "myemail@gmail.com"
+//         });
+//         console.log('my new user >>>', newUser);
+//     } catch (error) {
+//         console.log('new user was not created b/c of >>>', error);
+//     }
     
-}
-// @todo run createUser function below
+// }
+// // @todo run createUser function below
 
-// READ
-// find one user
-async function findOneUser() {
-    try {
-        const user = await db.user.findOne({
-            where: { id: 1 }
-        });
-        console.log('current user here >>>', user);  
-    } catch (error) {
-        console.log('did not find user b/c of >>>', error);
-    }
-}
-// @todo run findOneUser function below
+// // READ
+// // find one user
+// async function findOneUser() {
+//     try {
+//         const user = await db.user.findOne({
+//             where: { id: 1 }
+//         });
+//         console.log('current user here >>>', user);  
+//     } catch (error) {
+//         console.log('did not find user b/c of >>>', error);
+//     }
+// }
+// // @todo run findOneUser function below
 
-// find all users
-async function findAllUsers() {
-    try {
-        const users = await db.user.findAll();
-        console.log('all users here >>>', users);  
-    } catch (error) {
-        console.log('did not find all users because of >>>', error);
-    }
-}
-// @todo run findAllUsers function below
+// // find all users
+// async function findAllUsers() {
+//     try {
+//         const users = await db.user.findAll();
+//         console.log('all users here >>>', users);  
+//     } catch (error) {
+//         console.log('did not find all users because of >>>', error);
+//     }
+// }
+// // @todo run findAllUsers function below
 
-// find one user
-async function findOrCreate() {
-    try {
-        const users = await db.user.findOrCreate({
-            where: { email: 'brainsmith@gmail.com' },
-            defaults: {
-                name: 'Brian Smith',
-            },
-        });
-        console.log('all users here >>>', users);  
-    } catch (error) {
-        console.log('did not find all users because of >>>', error);
-    }
-}
-// @todo run findOrCreate function below
+// // find one user
+// async function findOrCreate() {
+//     try {
+//         const users = await db.user.findOrCreate({
+//             where: { email: 'brainsmith@gmail.com' },
+//             defaults: {
+//                 name: 'Brian Smith',
+//             },
+//         });
+//         console.log('all users here >>>', users);  
+//     } catch (error) {
+//         console.log('did not find all users because of >>>', error);
+//     }
+// }
+// // @todo run findOrCreate function below
 
-// UPDATE
-async function updateUser() {
-    try {
-        const numRowsUpdated = await db.user.update({
-            name: 'Brain Taco'
-        }, {
-            where: {
-                email: 'brainsmith@gmail.com'
-            }
-        });
-        console.log('number of users updated', numRowsUpdated);
-    } catch (error) {
-        console.log('did not update user(s) because of >>>', error);
-    }
-}
-// @todo run updateUser function below
+// // UPDATE
+// async function updateUser() {
+//     try {
+//         const numRowsUpdated = await db.user.update({
+//             name: 'Brain Taco'
+//         }, {
+//             where: {
+//                 email: 'brainsmith@gmail.com'
+//             }
+//         });
+//         console.log('number of users updated', numRowsUpdated);
+//     } catch (error) {
+//         console.log('did not update user(s) because of >>>', error);
+//     }
+// }
+// // @todo run updateUser function below
 
-// DELETE
-async function deleteUser() {
+// // DELETE
+// async function deleteUser() {
+//     try {
+//         let numOfRowsDeleted = await db.user.destroy({
+//             where: { email: 'brainsmith@gmail.com' }
+//         });
+//         console.log('number of rows deleted >>>', numOfRowsDeleted);
+//     } catch (error) {
+//         console.log('did not delete user(s) because of >>>', error);
+//     }
+// }
+// // @todo run deleteUser function below
+
+
+// db.favorite.create({
+//     favSong: 'Chiasma',
+//     album: 'Let the Sun Talk',
+//     artist: 'Mavi'
+//   }).then(song => {
+//     console.log('Created: ', song.favSong)
+//   })
+
+// db.favorite.findOne({
+//   where: {
+//     favSong: 'Chiasma'
+//   }
+// }).then(song => {
+//   console.log('Found: ', song.favSong)
+// })
+
+//create some favSongs with async/await syntax
+async function createFavoriteSong() {
     try {
-        let numOfRowsDeleted = await db.user.destroy({
-            where: { email: 'brainsmith@gmail.com' }
-        });
-        console.log('number of rows deleted >>>', numOfRowsDeleted);
-    } catch (error) {
-        console.log('did not delete user(s) because of >>>', error);
+      const newFavoriteSong = await db.favorite.create({ favSong: 'Nikes' })
+      console.log('the new FavoriteSong is:', newFavoriteSong)
+      const foundFavoriteSong = await db.favorite.findOne({
+        where: {
+          favSong: 'Nikes'
+        }
+      })
+      console.log('the found FavoriteSong is:', foundFavoriteSong)
+    } catch (err) {
+      console.log(err)
     }
-}
-// @todo run deleteUser function below
+  }
+  
+  createFavoriteSong()
